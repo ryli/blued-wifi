6145bf9f66f6c75599c2cfacdd0210c12e5eb611
ELEVEN <lov.2014@gmail.com>
2018-09-12 19:46:20 +0800

    feat: title

--- login.js
+++ login.js
@@ -1,5 +1,4 @@
 const puppeteer = require('puppeteer')
-const opn = require('opn')
 const spinner = require('./spinner')
 const hi = require('./hi')
 
@@ -7,17 +6,28 @@ const hi = require('./hi')
 const url = 'http://10.10.10.1/ac_portal/default/pc.html?tabs=pwd'
 const testUrl = 'https://www.baidu.com/'
 
+function loginFailed() {
+  spinner.setSpinnerTitle('login failed')
+  spinner.stop(true)
+  console.log('login failed')
+  process.exit()
+}
+
 async function login(username, pwd) {
-  const browser = await puppeteer.launch()
+  const opts = {}
+  // opts.headless = false
+  const browser = await puppeteer.launch(opts)
 
   const page = await browser.newPage()
 
-  await page.goto(url).catch(err => {})
+  await page.goto(url).catch(err => {
+    console.log('err', err)
+    loginFailed()
+  })
 
   const nameId = '#password_name'
   const pwdId = '#password_pwd'
   const btn = '#password_submitBtn'
-  const testInputId = '#kw'
 
   await page.waitForSelector(nameId)
   await page.type(nameId, username)
@@ -25,16 +35,21 @@ async function login(username, pwd) {
   await page.click(btn)
 
   // 打开网页 验证一下
-  await page.goto(testUrl).catch(err => {})
-  await page.waitForSelector(testInputId)
+  await page.goto(testUrl).catch(err => {
+    console.log('err', err)
+    loginFailed()
+  })
 
+  const title = await page.title();
   await browser.close()
-
-  spinner.setSpinnerTitle('login success')
-  spinner.stop(true)
-
-  // opn(testUrl, { wait: false })
-  console.log(hi)
+  if (title === '百度一下，你就知道') {
+    spinner.setSpinnerTitle('login success')
+    spinner.stop(true)
+    console.log(hi)
+    process.exit()
+  } else {
+    loginFailed()
+  }
 }
 
 module.exports = login
